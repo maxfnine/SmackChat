3d6b9c08bf10934e374b3af17643644c8bca776b
maxfnine <maxfnine@gmail.com>
2019-05-06 23:14:36 +0300

    Generating Avatar and background color in create user screen

--- app/src/main/java/com/example/smackchat/CreateUserActivity.kt
+++ app/src/main/java/com/example/smackchat/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.example.smackchat
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,10 +18,33 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(View: View){
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatarNumber = random.nextInt(28)
+
+        if(color==0){
+            userAvatar = "light$avatarNumber"
+        }else{
+            userAvatar = "dark$avatarNumber"
+        }
+
+        val resourceId = this.resources.getIdentifier(userAvatar,"drawable",this.packageName)
+        createAvatarImageView.setImageResource(resourceId)
 
     }
 
     fun generateColorClicked(view:View){
+        val random = Random()
+        val red = random.nextInt(256)
+        val green = random.nextInt(256)
+        val blue = random.nextInt(256)
+        createAvatarImageView.setBackgroundColor(Color.rgb(red,green, blue))
+
+        val savedRed = red.toDouble()/255
+        val savedGreen = green.toDouble()/255
+        val savedBlue = blue.toDouble()/255
+
+        avatarColor = "[$savedRed,$savedGreen,$savedBlue,1]"
 
     }
 
