02e8b7f5bc1fa106806e5526459424b8e0a8d12c
maxfnine <maxfnine@gmail.com>
2019-05-08 13:31:21 +0300

    login handling for existing user

--- app/src/main/java/com/example/smackchat/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/smackchat/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smackchat.R
+import com.example.smackchat.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,6 +16,19 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginButtonClicked(view: View){
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
+        AuthService.loginUser(this,email,password){
+            loginSuccess->
+            if(loginSuccess){
+                AuthService.findUserByEmail(this){
+                    userFound->
+                    if(userFound){
+                        finish()
+                    }
+                }
+            }
+        }
 
     }
 
