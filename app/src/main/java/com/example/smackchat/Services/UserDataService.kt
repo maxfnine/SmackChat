0e7552360c536989e9cc6b8de3ebe71279692a5e
maxfnine <maxfnine@gmail.com>
2019-05-08 09:13:54 +0300

    Logout handling after user creation

--- app/src/main/java/com/example/smackchat/Services/UserDataService.kt
+++ app/src/main/java/com/example/smackchat/Services/UserDataService.kt
@@ -1,10 +1,40 @@
 package com.example.smackchat.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
     var id = ""
     var avatarColor = ""
     var avatarName = ""
     var email = ""
     var name = ""
+
+    fun returnAvatarColor(components:String):Int{
+        val strippedColor = components.replace("[","").replace("]","").replace(","," ")
+        var red = 0
+        var green = 0
+        var blue = 0
+
+        val scanner = Scanner(strippedColor)
+        if(scanner.hasNext()){
+            red = (scanner.nextDouble() * 255).toInt()
+            green = (scanner.nextDouble() * 255).toInt()
+            blue = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(red,green,blue)
+    }
+
+    fun logout(){
+        id = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.isLoggedIn = false
+        AuthService.userEmail = ""
+    }
 
 }
